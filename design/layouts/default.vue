d29d5524963712aa9f7a42285051f47b288f3314
Jonas Rosenkilde <59253@edu.campusvejle.dk>
2024-02-02 11:23:13 +0100

    upsætning

---
+++ design/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <nav></nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+
+    <footer></footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ design/pages/products/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
