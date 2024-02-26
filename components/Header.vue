295467ee90f98f3f1dc195442af9ca256bbf7db0
Foxistyle <59706@edu.campusvejle.dk>
2024-02-26 14:58:25 +0100

    header update

--- components/Header.vue
+++ components/Header.vue
@@ -1,4 +1,25 @@
 <template>
+  <div class="card card1">
+    <div class="search iconcard">
+      <i class="searchicon pi pi-search">
+        <input class="searchinput" type="text" />
+      </i>
+    </div>
+    <div class="logo">
+      <NuxtLink to="/">
+        <img src="http://placehold.it/250x60?text=hello+world" alt="" />
+      </NuxtLink>
+    </div>
+
+    <div class="flex">
+      <i class="shoppingbag pi pi-shopping-bag"></i>
+      <div class="login iconcard">
+        <i class="userlogo pi pi-user"></i>
+
+        <p>Login</p>
+      </div>
+    </div>
+  </div>
   <div class="card">
     <Menubar :model="items">
       <template #item="{ item, props }">
@@ -16,10 +37,6 @@
         </NuxtLink>
       </template>
     </Menubar>
-    <div class="login">
-      <img src="https://unsplash.it/40/40" alt="Profil" />
-      <p>Login</p>
-    </div>
   </div>
 </template>
 <script setup>
@@ -38,26 +55,92 @@ const items = ref([
     label: "Products",
     to: "/products?item",
   },
+  {
+    label: "Gift Boxes",
+    to: "/GiftBoxes",
+  },
+  {
+    label: "Gift Card",
+    to: "/GiftCard",
+  },
+  {
+    label: "About Us",
+    to: "/AboutUs",
+  },
+  {
+    label: "Contact",
+    to: "/Contact",
+  },
+  {
+    label: "Blog",
+    to: "/blog",
+  },
 ]);
 </script>
 
 <style scoped>
+.userlogo {
+  font-size: 1.5rem;
+  border-radius: 999px;
+}
+
 .router-link-active {
   color: hsl(164, 82%, 39%);
 }
+
 .card {
-  display: flex;
-  justify-content: space-between;
-}
-.login {
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  margin-right: 50px;
-  border: 2px;
-  border-radius: 10px;
-  border-color: black;
-  border-style: solid;
+}
+
+.card1 {
+  display: none;
+  justify-content: space-between;
+  margin-top: 1.3rem;
+}
+
+.searchicon {
+  margin-left: 5px;
+}
+
+.searchinput {
+  width: 87%;
+}
+
+.shoppingbag {
+  margin-top: 1.3rem;
+  margin-right: 2rem;
+  font-size: 1.7rem;
+}
+
+.search {
+  height: 2vw;
+  width: 15vw;
+  margin-top: 10px;
+  margin-left: 50px;
+}
+
+.login {
+  height: 4vw;
   width: 10vw;
+  margin-right: 50px;
+}
+
+.iconcard {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border-color: black;
+  border: 2px;
+  border-style: solid;
+  border-radius: 10px;
+  margin-top: 10px;
+}
+
+@media screen and (min-width: 962px) {
+  .card1 {
+    display: flex;
+  }
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,6 +10,8 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import "primeicons/primeicons.css";
+</script>
 
 <style scoped></style>
