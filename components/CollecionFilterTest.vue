31cb48a8495f42c6bf91c59bab41649805599cea
Jonas Rosenkilde <59253@edu.campusvejle.dk>
2024-03-04 09:43:29 +0100

    Filter update

---
+++ components/CollecionFilterTest.vue
@@ -0,0 +1,39 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+// import { ref } from "vue";
+
+// const products = ref([]);
+
+// const itemFromUrl = router.currentRoute.value.query.item;
+
+// fetch("https://fakestoreapi.com/products")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     if (itemFromUrl === "mand") {
+//       products.value = data.filter(
+//         (product) => product.category === "men's clothing"
+//       );
+//     } else if (itemFromUrl === "kvinde") {
+//       products.value = data.filter(
+//         (product) => product.category === "women's clothing"
+//       );
+//     } else if (itemFromUrl === "jewelery") {
+//       products.value = data.filter(
+//         (product) => product.category === "jewelery"
+//     } else if (itemFromUrl === "electronics") {
+//       products.value = data.filter(
+//         (product) => product.category === "electronics"
+//     } else if (itemFromUrl === "new") {
+//       products.value = data.filter((product) => product.category === "new");
+//     } else {
+//       products.value = data;
+//     }
+//     console.dir(products.value);
+//   })
+//   .catch((error) => console.error(error));
+</script>
+
+<style scoped></style>
