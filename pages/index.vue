3cb4f18261dcadf8fffa1e7500a82b47f80e65d3
klementj <222317+klementj@users.noreply.github.com>
2024-03-06 10:16:18 +0100

    Change index page to show exmaple of loading products

--- pages/index.vue
+++ pages/index.vue
@@ -1,194 +1,51 @@
-<template>
-  <div class="herobillede">
-    <img src="https://unsplash.it/1920/300" alt="Hero-billede" />
-    <p class="herotext">
-      We make clothes with love, designed to spark the conversation, and support
-      animal rescue too
-    </p>
-  </div>
-  <h2>New Products</h2>
-  <div class="cards">
-    <Carousel
-      :value="products"
-      :numVisible="5"
-      :numScroll="1"
-      :responsiveOptions="responsiveOptions"
-      circular
-      :autoplayInterval="3000"
-      :showIndicators="false"
-      class="card"
-    >
-      <template #item="slotProps">
-        <div
-          class="border-1 surface-border border-round m-2 p-3 item itemCenter"
-        >
-          <div class="mb-3 image align-middle imgboks">
-            <div class="relative">
-              <img
-                :src="slotProps.data.image"
-                :alt="slotProps.data.name"
-                class="w-full border-round thumb"
-              />
-            </div>
-          </div>
-          <div class="mb-3 font-medium">
-            {{ slotProps.data.name }}
-          </div>
-          <div
-            class="flex justify-content-between align-items-center flex-col buywidth"
-          >
-            <p class="font-bold text-gray-500 m-4 truncate flextext">
-              {{ slotProps.data.title }}
-            </p>
-            <div class="font-bold text-gray-500 m-4 truncate textcenter">
-              Price - ${{ slotProps.data.price }}
-            </div>
-            <span>
-              <NuxtLink :to="`/products/${slotProps.data.id}`">
-                <p class="btn my-4 text-center">View Details</p>
-              </NuxtLink>
-            </span>
-          </div>
-        </div>
-      </template>
-    </Carousel>
-  </div>
-</template>
-
 <script setup>
-import Carousel from "primevue/carousel";
-import { ref, onMounted } from "vue";
+const { data: products, pending, error } = await useFetch('/api/products')
 
-const products = ref([]);
-
-const responsiveOptions = ref([
-  {
-    breakpoint: "1400px",
-    numVisible: 5,
-    numScroll: 1,
-  },
-  {
-    breakpoint: "1199px",
-    numVisible: 5,
-    numScroll: 1,
-  },
-  {
-    breakpoint: "767px",
-    numVisible: 2,
-    numScroll: 1,
-  },
-  {
-    breakpoint: "575px",
-    numVisible: 1,
-    numScroll: 1,
-  },
-]);
-
-onMounted(async () => {
+const reloadProducts = async () => {
+  pending.value = true
+  error.value = null
   try {
-    const response = await fetch("https://fakestoreapi.com/products");
-    if (!response.ok) {
-      throw new Error("Failed to fetch products");
-    }
-    const data = await response.json();
-    const sortedProducts = data
-      .sort((a, b) => b.timestamp - a.timestamp)
-      .slice(0, 10);
-    products.value = sortedProducts;
-  } catch (error) {
-    console.error(error);
+    const response = await $fetch('/api/products')
+    const data = await response
+    products.value = data
+    pending.value = false
+  } catch (err) {
+    error.value = err
+    pending.value = false
   }
-});
+}
 </script>
 
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-h2 {
-  padding: 20px;
-  text-align: center;
-  font-weight: bold;
-}
-.cards {
-  display: flex;
-  justify-content: center;
-  padding-bottom: 50px;
-}
-.card {
-  display: flex;
-  justify-content: center;
-  max-width: 80vw;
-  padding: 20px 0;
-}
-.p-carousel-content {
-  justify-content: center;
-}
-.item {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 0 1rem;
-  height: 245px;
-  max-width: 235px;
-}
+<template>
+  <div class="p-2 md:p-6 lg:p-10">
+    <Button @click="reloadProducts" label="Reload products"  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" />
+  
+    <!-- Når produkter er ved at loade -->
+    <!-- Her er det godt at bruge et skeleton component -->
+    <!-- https://tailwind.primevue.org/skeleton/ -->
+    <div v-if="pending">Loading...</div>
+    <div v-else-if="error">Error loading products.</div>
 
-.thumb {
-  display: flex;
-  max-height: 150px;
-  max-width: 150px;
-}
-.herobillede {
-  width: 100%;
-}
-.carousel img {
-  padding: 20px;
-}
-.p-carousel-item {
-  display: flex;
-  justify-content: center;
-}
-.buybutton {
-  width: 100%;
-  height: 35px;
-}
-.buywidth {
-  width: 100%;
-}
-.imgboks {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-}
-.textcenter {
-  text-align: center;
-}
+    <!-- Produkter hvis de loades -->
+    <div v-else class="flex flex-wrap gap-6 ">
+      <article  v-for="product in products.products" :key="product.id" class="bg-white rounded-lg overflow-hidden shadow-md max-w-sm">
+        <img class="w-full" :src="product.thumbnail">
+        <div class="p-4">
+            <h3 class="text-lg font-medium mb-2">
+              {{ product.title }}
+            </h3>
+            <p class="text-gray-600 text-sm mb-4">
+              {{ product.description }}
+            </p>
+        </div>
+      </article>
+    </div>
 
-.herobillede {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-}
-.herotext {
-  display: flex;
-  position: absolute;
-  width: 50%;
-  top: calc(50% - 40px);
-  color: white;
-  font-size: 40px;
-  line-height: 40px;
-  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
-    1px 1px 0 #000;
-  text-align: center;
-}
-.itemCenter {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+
+    <pre>
+      {{ products.products }}
+    </pre>
+
+
+  </div>
+</template>
